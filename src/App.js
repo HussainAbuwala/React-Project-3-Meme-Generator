36b797c7b80ca1291b4f878b2152da8ecdc03f65
Hussain <hussainabuwala.1997@gmail.com>
2023-06-26 11:53:03 +0600

    Header component setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react"
+import Header from "./components/Header"
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Helllo world</h1>
+    <div>
+      <Header />
     </div>
   );
 }
